3d71cbd51f6f0279b06390091eda5f729cb693f4
Nova Wolfcreek <novawko@gmail.com>
2025-02-20 21:40:03 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src="https://images2.imgbox.com/cf/2e/bpyLZwLN_o.png" className="block w-50 h-50 mx-auto mb-4 rounded-full" />
+      <img src="https://images2.imgbox.com/cf/2e/bpyLZwLN_o.png" className="block w-20 h-20 mx-auto mb-4 rounded-full" />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
           {name}
